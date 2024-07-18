ac47a3bd3004429c58f16ee1784c9432f27f2760
akanksha <akankshakushwah1501@gmail.com>
2024-07-18 12:32:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  const name = "Akanksha"
+  res.send(`Hello from home, ${name}`)
+})
+
+app.get('/services', (req, res) => {
+  res.send("Hello from services page")
+})
+
+app.get('/courses', (req, res) => {
+  const data = [
+    { id: 1, course: "MEAN" },
+    { id: 2, course: "MERN" },
+    { id: 3, course: "MERP" },
+    { id: 4, course: "DSA" },
+  ]
+  res.send(data)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://${hostname}:${port}`)
+})
